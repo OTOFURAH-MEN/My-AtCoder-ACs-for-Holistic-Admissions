ef07755b496d9b7b8375e72dc601d520cefe71ad
OTOFURAH-MEN <dai2008bottle@gmail.com>
2026-08-28 12:14:20 +0900

    Create ABC469-A.py

---
+++ ABC469-A.py
@@ -0,0 +1,11 @@
+N,K=map(int,input().split())
+print(N-K+1)
+
+#ABC468-B Corridor Watch
+#問題文
+#[AtCoder ABC469 A](https://atcoder.jp/contests/abc469/tasks/abc469_a)
+#提出結果
+#[提出#78001044](https://atcoder.jp/contests/abc469/submissions/78001044)
+#推定計算量
+#Time:O(1)
+#Space: O(1)
