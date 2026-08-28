34b4b594a66643c4a4c6f8b639e16480c6032725
OTOFURAH-MEN <dai2008bottle@gmail.com>
2026-08-28 12:11:22 +0900

    Update ABC468-B.py

--- ABC468-B.py
+++ ABC468-B.py
@@ -15,3 +15,14 @@ for i in range(M):
             not_see+=1
 print(not_see)
 
+#ABC468-B Corridor Watch
+#問題文
+#[AtCoder ABC468 B](https://atcoder.jp/contests/abc468/tasks/abc468_b)
+#提出結果
+#[提出#77841042](https://atcoder.jp/contests/abc468/submissions/77841042)
+
+#解法解説
+#廊下について線形探索を行い、それぞれのガードマンGがいない部屋について、その部屋を中心とする視界D分の部屋をスライスし、それらにガードマンがいないならばnot_seeを1増やした。
+#推定計算量
+#Time:O(M^2)
+#Space: O(M^2)
